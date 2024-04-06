43b2ccd544726b212fe269a1ec1574005c399871
fernandes-wiraharjo <fernandeswiraharjo@gmail.com>
2024-04-06 22:03:15 +0700

    progress add customer type

--- database/migrations/customers/2014_10_12_000010_create_customers_table.php
+++ database/migrations/customers/2014_10_12_000010_create_customers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
     Schema::create('customers', function (Blueprint $table) {
       $table->id();
       $table->string('name', 50);
+      $table->string('type', 50); //user, toko
       $table->string('address', 250);
       $table->string('phone_no', 30);
       $table->string('pic_name', 50);
--- resources/views/content/masters/master-customer-list.blade.php
+++ resources/views/content/masters/master-customer-list.blade.php
@@ -60,6 +60,7 @@
         <tr>
           <th></th>
           <th>Name</th>
+          <!-- <th>Type</th> -->
           <th>Address</th>
           <th>Phone No</th>
           <th>Pic Name</th>
@@ -82,6 +83,14 @@
         <div class="mb-3">
           <label class="form-label" for="add-customer-name">Name</label>
           <input type="text" class="form-control" id="add-customer-name" placeholder="name" name="name" aria-label="name" />
+        </div>
+        <div class="mb-3">
+          <label class="form-label" for="type">Type</label>
+          <select id="type" name="type" class="form-select">
+            <option value="">Select</option>
+            <option value="user">user</option>
+            <option value="toko">toko</option>
+          </select>
         </div>
         <div class="mb-3">
           <label class="form-label" for="add-customer-phone">Phone No</label>
